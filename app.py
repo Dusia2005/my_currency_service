f128b5b347b9747c27d4bc5cba713d39cbf66d27
Dasha Leshok <dashaleshok01@gmail.com>
2024-06-15 18:09:09 +0300

    реализация микросервиса, получение и отображение курсов валют с сайта, запись информации в БД

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Depends
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from currency_service.api import router as api_router
+from currency_service.database import init_db
 
 app = FastAPI()
+app.include_router(api_router)
+
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-def read_root():
-    html_content = "<h2>Hello Word!</h2>"
-    return HTMLResponse(content=html_content)
+@app.on_event("startup")
+def on_startup():
+    init_db()
 
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ currency_service/utils.py
@@ -0,0 +1,40 @@
+import requests
+from datetime import datetime
+from sqlalchemy.orm import Session
+from currency_service.database import CurrencyRate
+
+
+def fetch_currency_rates(date: str):
+    url = f'https://www.nbrb.by/api/exrates/rates?ondate={date}&periodicity=0'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+
+def fetch_currency_rate_by_code(date: str, code: str):
+    url = f'https://www.nbrb.by/api/exrates/rates/{code}?ondate={date}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+
+def save_currency_rates(db: Session, rates: list, date: str):
+    date_obj = datetime.strptime(date, '%Y-%m-%d').date()
+    for rate in rates:
+        currency_rate = CurrencyRate(
+            date=date_obj,
+            currency_code=rate['Cur_Abbreviation'],
+            rate=rate['Cur_OfficialRate']
+        )
+        db.add(currency_rate)
+    db.commit()
+
+
+def get_currency_rate(db: Session, date: str, code: str):
+    date_obj = datetime.strptime(date, '%Y-%m-%d').date()
+    return db.query(CurrencyRate).filter(
+        CurrencyRate.date == date_obj,
+        CurrencyRate.currency_code == code
+    ).first()
